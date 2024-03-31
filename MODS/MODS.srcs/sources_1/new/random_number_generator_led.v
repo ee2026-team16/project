8c1b0b9f070eee2540986847af61cbbf7fa0bf4f
gnoynuj <50942501+gnoynuj@users.noreply.github.com>
2024-03-31 13:05:58 +0800

    Update random_number_generator_led.v

--- MODS/MODS.srcs/sources_1/new/random_number_generator_led.v
+++ MODS/MODS.srcs/sources_1/new/random_number_generator_led.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 23.03.2024 20:10:04
+// Create Date: 03/26/2024 05:59:20 PM
 // Design Name: 
 // Module Name: random_number_generator_led
 // Project Name: 
@@ -20,12 +20,11 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module random_number_generator_led(input basys_clock, input [31:0] prev_number, output reg [31:0] new_number);
+module random_number_generator_led(input basys_clock, input [15:0] prev_number, output reg [15:0] new_number);
+
     always @ (posedge basys_clock)
     begin
-        new_number = prev_number ^ ((prev_number << 13 & 32'hFFFFFFFF));
-        new_number = new_number ^ ((prev_number >> 17 & 32'hFFFFFFFF));
-        new_number = new_number ^ ((prev_number << 5 & 32'hFFFFFFFF));
+        new_number[15:0] <= {(prev_number[5]^prev_number[3]^prev_number[2]^prev_number[0]),prev_number[12:1],prev_number[15:13]};     
     end
     
 endmodule
